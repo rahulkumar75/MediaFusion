e4ff4efd72ba2a6b8da307ef7870a7e3bd79e2a0
Rahul Kumar <rahulkumarx333@gmail.com>
2024-08-29 18:53:37 +0530

    Add more Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,36 +1,53 @@
-import { Router } from "express"
-import { 
-    registerUser,
-    loginUser,
-    logoutUser,
-    refreshAccessToken,
-
-} from "../controllers/user.controller.js"
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+  updateAccountDetails,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {upload} from "../middlewares/multer.middleware.js" 
-
-
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    )
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-    router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-    //SECURED routes
-    router.route("/logout").post(verifyJWT,  logoutUser)
-    router.route("/referesh-token").post(refreshAccessToken)
+//SECURED routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/referesh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-export default router
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+export default router;
